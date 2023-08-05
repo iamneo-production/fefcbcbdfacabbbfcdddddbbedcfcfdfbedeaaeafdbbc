a329142a7158fc14d99dceb04e5d9eca6741c5ad
Iamneo <techadmin@iamneo.ai>
2023-08-05 07:33:15 +0000

    Backup Commit

---
+++ reactapp/src/functions/quizFunctions.js
@@ -0,0 +1,27 @@
+// quizFunctions.js
+const quizData = [
+    {
+        text: 'Question 1',
+        options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+        correctOption: 'Option 2',
+    },
+    {
+        text: 'Question 2',
+        options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+        correctOption: 'Option 3',
+    },
+    // Add more questions as needed...
+    ];
+                                                            
+    export const getQuizData = () => {
+    // You can fetch quiz data from an API here instead
+    return quizData;
+    };
+    export const checkAnswer = (quizData, selectedOption) => {
+        const currentQuestion = quizData.find((question) => question.options.includes(selectedOption));
+        if (!currentQuestion) return false;
+                                                                                                                                                                                                                                                                    return currentQuestion.correctOption === selectedOption;
+                                                                                                                                                                                                                                                                                                                                                              };
+    }                                                                   
+                                                                            
+                                                                                            

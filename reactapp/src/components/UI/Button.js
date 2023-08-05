13648a41d7027ea678bf69b10edf52ad64764639
Iamneo <techadmin@iamneo.ai>
2023-08-05 08:03:15 +0000

    Backup Commit

--- reactapp/src/components/UI/Button.js
+++ reactapp/src/components/UI/Button.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 const Button = ({ onClick, disabled, children }) => {
   return (
-      <button onClick={onClick} disabled={disabled}>
-            {children}
-                </button>
-                  );
-                  };
+    <button onClick={onClick} disabled={disabled}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
---
+++ reactapp/src/components/UI/Input.js
@@ -0,0 +1,8 @@
+//Input.js
+import React from 'react';
+
+const Input = ({ type, value, onChange }) => {
+  return <input type={type} value={value} onChange={onChange} />;
+  };
+
+  export default Input;

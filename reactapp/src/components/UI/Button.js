c40ef9a3582d9632ca830801c3f68016c2364c47
Iamneo <techadmin@iamneo.ai>
2023-08-05 07:20:29 +0000

    Backup Commit

---
+++ reactapp/src/components/UI/Button.js
@@ -0,0 +1,12 @@
+// Button.js
+import React from 'react';
+
+const Button = ({ onClick, disabled, children }) => {
+  return (
+      <button onClick={onClick} disabled={disabled}>
+            {children}
+                </button>
+                  );
+                  };
+
+export default Button;
---
+++ reactapp/src/components/UI/Card.js
@@ -0,0 +1,8 @@
+// Card.js
+import React from 'react';
+
+const Card = ({ children }) => {
+  return <div className="card">{children}</div>;
+  };
+
+export default Card;

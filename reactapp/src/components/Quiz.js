cbb7d669f66277485d04c30ccd1f5b80c520fce0
Iamneo <techadmin@iamneo.ai>
2023-08-13 17:47:46 +0000

    Backup Commit

--- reactapp/src/components/Quiz.js
+++ reactapp/src/components/Quiz.js
@@ -1,95 +1,55 @@
 import React, { useState, useEffect } from 'react';
-import Card from './UI/Card/Card';
-import Button from './UI/Button/Button';
+import Card from './UI/Card';
+import Button from './UI/Button';
+import { questions } from '../data'; // Replace with your trivia questions data
 import { checkAnswers } from '../functions/quizFunctions';
 
-const sampleQuestions = [
-    {
-        question: 'What is the capital of France?',
-        options: ['Berlin', 'Paris', 'London', 'Rome'],
-        correctAnswer: 'Paris',
-    },
-    {
-        question: 'Which planet is known as the Red Planet?',
-        options: ['Venus', 'Mars', 'Jupiter', 'Saturn'],
-        correctAnswer: 'Mars',
-    },
-    {
-        question: 'What gas do plants use for photosynthesis?',
-        options: ['Carbon Dioxide', 'Oxygen', 'Nitrogen', 'Hydrogen'],
-        correctAnswer: 'Carbon Dioxide',
-    },
-    {
-        question: 'Which famous scientist developed the theory of relativity?',
-        options: ['Isaac Newton', 'Galileo Galilei', 'Albert Einstein', 'Nikola Tesla'],
-        correctAnswer: 'Albert Einstein',
-    },
-    {
-        question: 'What is the chemical symbol for gold?',
-        options: ['Go', 'Au', 'Ag', 'Gl'],
-        correctAnswer: 'Au',
-    },
-];
-
 const Quiz = () => {
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [selectedOption, setSelectedOption] = useState('');
-    const [disabledOptions, setDisabledOptions] = useState([]);
-    const [score, setScore] = useState(0);
+    const [selectedOptions, setSelectedOptions] = useState({});
     const [showResults, setShowResults] = useState(false);
-    const [randomQuestions, setRandomQuestions] = useState([]);
+    const [score, setScore] = useState(0);
 
-    useEffect(() => {
-        const shuffledQuestions = sampleQuestions.sort(() => 0.5 - Math.random());
-        const selectedQuestions = shuffledQuestions.slice(0, 5); // Select 5 random questions
-        setRandomQuestions(selectedQuestions);
-    }, []);
-
-    const currentQuestion = randomQuestions[currentQuestionIndex];
-
-    const handleOptionClick = (option) => {
-        setSelectedOption(option);
-        setDisabledOptions([...disabledOptions, option]);
+    const handleOptionClick = (questionId, option) => {
+        setSelectedOptions({
+            ...selectedOptions,
+            [questionId]: option,
+        });
     };
 
-    const handleNextQuestion = () => {
-        if (checkAnswers(selectedOption, currentQuestion.correctAnswer)) {
-            setScore(score + 1);
-        }
-
-        setSelectedOption('');
-        setDisabledOptions([]);
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-
-        if (currentQuestionIndex === randomQuestions.length - 1) {
-            setShowResults(true);
-        }
+    const handleShowResults = () => {
+        let correctAnswers = 0;
+        questions.forEach((question) => {
+            if (selectedOptions[question.questionId] === question.correctAnswer) {
+                correctAnswers++;
+            }
+        });
+        setScore(correctAnswers);
+        setShowResults(true);
     };
-
-    if (!currentQuestion) {
-        return <div>Loading...</div>; // Handle loading state
-    }
 
     return (
         <div>
-            {!showResults && (
-                <Card>
-                    <h2>{currentQuestion.question}</h2>
-                    {currentQuestion.options.map((option, index) => (
+            <h1>React Quiz Application</h1>
+            {questions.map((question) => (
+                <Card key={question.questionId}>
+                    <h2>{question.question}</h2>
+                    {question.options.map((option, index) => (
                         <Button
                             key={index}
                             text={option}
-                            onClick={() => handleOptionClick(option)}
-                            disabled={disabledOptions.includes(option)}
+                            onClick={() => handleOptionClick(question.questionId, option)}
+                            disabled={showResults}
                         />
                     ))}
-                    <Button text="Next Question" onClick={handleNextQuestion} disabled={!selectedOption} />
                 </Card>
+            ))}
+            {!showResults && (
+                <Button text="Show Results" onClick={handleShowResults} />
             )}
             {showResults && (
                 <Card>
                     <h2>Quiz Results</h2>
-                    <p>Your score: {score}</p>
+                    <p>Your score: {score} out of {questions.length}</p>
                 </Card>
             )}
         </div>
---
+++ reactapp/src/components/data.js
@@ -0,0 +1,34 @@
+export const questions = [
+    {
+        questionId: 1,
+        question: 'What is the capital of France?',
+        options: ['Berlin', 'Paris', 'London', 'Rome'],
+        correctAnswer: 'Paris',
+    },
+    {
+        questionId: 2,
+        question: 'Which planet is known as the Red Planet?',
+        options: ['Venus', 'Mars', 'Jupiter', 'Saturn'],
+        correctAnswer: 'Mars',
+    },
+    {
+        questionId: 3,
+        question: 'What gas do plants use for photosynthesis?',
+        options: ['Carbon Dioxide', 'Oxygen', 'Nitrogen', 'Hydrogen'],
+        correctAnswer: 'Carbon Dioxide',
+    },
+    {
+        questionId: 4,
+        question: 'Which famous scientist developed the theory of relativity?',
+        options: ['Isaac Newton', 'Galileo Galilei', 'Albert Einstein', 'Nikola Tesla'],
+        correctAnswer: 'Albert Einstein',
+    },
+    {
+        questionId: 5,
+        question: 'What is the chemical symbol for gold?',
+        options: ['Go', 'Au', 'Ag', 'Gl'],
+        correctAnswer: 'Au',
+    },
+    // Add more questions as needed
+];
+                                                                                                          

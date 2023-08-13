90bdcebb27ff6f759b1a5f14516205218711ea76
Iamneo <techadmin@iamneo.ai>
2023-08-13 16:07:27 +0000

    Backup Commit

---
+++ reactapp/src/components/UI/Button/Button.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Button from "./Button"; // Assuming you have a Button component
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quizStarted: false,
+            showResults: false
+        };
+    }
+
+    startQuiz = () => {
+        this.setState({
+            quizStarted: true
+        });
+    };
+
+    showResults = () => {
+        this.setState({
+            showResults: true
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Quizz App</h1>
+                {!this.state.quizStarted && (
+                    <button onClick={this.startQuiz}>Start Quiz</button>
+                )}
+                {this.state.quizStarted && !this.state.showResults && (
+                    // Render the quiz questions here
+                    <Button onClick={() => { /* Handle question logic */ }}>
+                        Question
+                    </Button>
+                )}
+                {this.state.quizStarted && this.state.showResults && (
+                    <button onClick={this.showResults}>Show Results</button>
+                )}
+            </div>
+        );
+    }
+}
+
+export default App;
